f0a70d80753652e8a141d7dda3138f093b772fa7
sajwan05 <surajsajwan328@gmail.com>
2026-09-18 14:11:38 +0530

    build square grid sketch

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ const wrapper = document.querySelector(".wrapper");
 
 for(let i = 0; i < 256; i++){
     const div = document.createElement("div");
+    div.classList.add("child");
     div.setAttribute("style", "border: 2px solid black; width: 1rem; height:1rem;");
 
     wrapper.appendChild(div);
 }
+
+wrapper.addEventListener("mouseover", changeColor);
+
+function changeColor(e){
+    const target = e.target;
+
+    target.setAttribute("style", "background: #f00;");
+
+}
+
 
