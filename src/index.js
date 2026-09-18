6b4f404766ac2b26e4d90c8938108d8aeedc0f87
sajwan05 <surajsajwan328@gmail.com>
2026-09-18 14:34:41 +0530

    create dynamic grid boxes based on grid size entered by user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const wrapper = document.querySelector(".wrapper");
 
+const button = document.querySelector(".sizeGrid");
+let number;
+
+button.addEventListener("click", (e) => {
+    number = parseInt(prompt("Please enter the size of the grid: "));
+     wrapper.textContent = "";
+    for(let i = 0; i < (number * number); i++){
+        const div = document.createElement("div");
+        div.classList.add("dynamic-child");
+        div.setAttribute("style", "border: 2px solid black;");
+       
+        wrapper.appendChild(div);
+    }
+
+
+});
+
 for(let i = 0; i < 256; i++){
     const div = document.createElement("div");
     div.classList.add("child");
